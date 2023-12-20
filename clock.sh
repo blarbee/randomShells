d4aabc8d90faf341e2521e98a8b713d41ae11476
blarbee <nwl.ouachen@gmail.com>
2023-12-20 21:55:59 +0100

    mhm

--- clock.sh
+++ clock.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
 
-# Set the initial time to 00:00:00
-current_time="00:00:00"
-
-# Calculate the total number of seconds in 48 hours
-total_seconds=$((48 * 60 * 60))
-
-# Function to increment the time
-increment_time() {
-    current_seconds=$(date -d "$current_time" +%s)
-    new_seconds=$((current_seconds + 1))
-    current_time=$(date -u -d @"$new_seconds" +%H:%M:%S)
+# Function to perform cleanup before exiting
+cleanup() {
+    echo -e "\nExiting the 48-hour clock script."
+    exit
 }
 
-# Run the clock for 48 hours
-for ((i=0; i<$total_seconds; i++)); do
-    # Print the current time
-    printf "\r%s" "$current_time"
+# Trap the SIGINT signal (Ctrl + C) and call the cleanup function
+trap cleanup SIGINT
 
-    # Wait for one second
+
+while true; do
+
+
+    current_time=$(date +"%H:%M:%S")
+    echo $current_time
+
+    day=$(date + "%d")
+    hours=$(date +"%H")
+    minutes=$(date +"%M")
+    seconds=$(date +"%S")
+
+    if (day%2==0) then; do
+        hours += 24
+
+
+    fi
+
+    total_minutes=$((hours * 60 + minutes))
+    total_minutes_48=$((total_minutes * 48 / 24))
+
+
+    hours_48=$((total_minutes_48 / 60))
+    minutes_48=$((total_minutes_48 % 60))
+
+    echo -ne "\r48-Hour Clock: $hours_48:$minutes_48:$seconds"
+
     sleep 1
-
-    # Increment the time by one second
-    increment_time
 done
-
-# Print a new line at the end
-echo
 
