83d9c401ad769604df97505b4d3221e5374f2d77
blarbee <nwl.ouachen@gmail.com>
2023-12-21 12:11:07 +0100

    done with the 48h clock

--- clock.sh
+++ clock.sh
@@ -9,33 +9,23 @@ cleanup() {
 # Trap the SIGINT signal (Ctrl + C) and call the cleanup function
 trap cleanup SIGINT
 
-
 while true; do
+    current_time=$(date +"%d:%H:%M:%S")
+  
 
-
-    current_time=$(date +"%H:%M:%S")
-    echo $current_time
-
-    day=$(date + "%d")
+    day=$(date +"%d")
     hours=$(date +"%H")
     minutes=$(date +"%M")
     seconds=$(date +"%S")
 
-    if (day%2==0) then; do
-        hours += 24
-
-
+    if [ $((day % 2)) -eq 0 ]; then
+        hours=$((hours + 24))   
+        echo -ne "\r48-Hour Clock: $hours:$minutes:$seconds"
+    else
+        echo -ne "\r48-Hour Clock: $hours:$minutes:$seconds"
     fi
 
-    total_minutes=$((hours * 60 + minutes))
-    total_minutes_48=$((total_minutes * 48 / 24))
-
-
-    hours_48=$((total_minutes_48 / 60))
-    minutes_48=$((total_minutes_48 % 60))
-
-    echo -ne "\r48-Hour Clock: $hours_48:$minutes_48:$seconds"
+    echo -ne "\r48-Hour Clock: $hours:$minutes:$seconds"
 
     sleep 1
 done
-
